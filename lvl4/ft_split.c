590a60db8fe58005be6dba46452cc380969267f0
Arthur DU PELOUX <adu-pelo@e1r3p17.42.fr>
2016-01-26 14:24:59 +0100

    ft_split

---
+++ lvl4/ft_split.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adu-pelo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/26 13:46:14 by adu-pelo          #+#    #+#             */
+/*   Updated: 2016/01/26 14:24:12 by adu-pelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h> // printf
+
+int		count_word(char *str)
+{
+	int i;
+	int cnt;
+
+	i = 0;
+	cnt = 0;
+	while (str[i])
+	{
+		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+			i++;
+		if (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+		{
+			while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+				i++;
+			cnt++;
+		}
+	}
+	return (cnt);
+}
+
+int		word_len(char *str, int i)
+{
+	int cnt;
+
+	cnt = 0;
+	while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+	{
+		cnt++;
+		i++;
+	}
+	return (cnt);
+}
+
+char 	**ft_split(char *str)
+{
+	int i;
+	int j;
+	int k;
+	char **tab;
+
+	i = 0;
+	j = 0;
+	if (!(tab = malloc(sizeof(char *) * (count_word(str) + 1))))
+		return (NULL);
+	while (str[i])
+	{
+		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+			i++;
+		if (str[i])
+		{
+			k = 0;
+			if (!(tab = malloc(sizeof(char) * (word_len(str, i) + 1))))
+				return (NULL);
+			while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+			{
+				tab[j][k++] = str[i++]; // segfault 
+				printf("d\n");
+			}
+			tab[j++][k] = '\0';
+		}
+	}
+	tab[j] = NULL;
+	return (tab);
+}
+
+int		main(void)
+{
+	int i = 0;
+	char str[] = "bonjour je m'appel Arthur";
+	char **tab = ft_split(str);
+
+	printf("a\n");
+	while (i < 4)
+	{
+		printf("string %d : %s", i, tab[i]);
+		i++;
+	}
+	return (0);
+}
